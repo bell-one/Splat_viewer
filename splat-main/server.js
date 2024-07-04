62e9cd291843c290f2b8d087a141524720d8c8d0
a1raman <choikhss1@gmail.com>
2024-07-04 16:35:17 +0900

    Add files via upload

---
+++ splat-main/server.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '0.0.0.0'; // 이는 모든 네트워크 인터페이스에서 접근 가능하도록 설정합니다.
+const port = 20022; // 포트 번호는 원하는 번호로 설정합니다.
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html'; // 기본적으로 index.html을 제공합니다.
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const contentType = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.woff2': 'application/font-woff2',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'image/svg+xml'
+  }[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
