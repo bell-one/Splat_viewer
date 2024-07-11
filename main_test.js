ccedc5d85bb3ff77caea0bd865774bb942aacc63
bell-one <bellone1991@gmail.com>
2024-07-11 19:10:26 +0900

    Update main_test.js

--- main_test.js
+++ main_test.js
@@ -646,7 +646,6 @@ let defaultViewMatrix = [
 
 let viewMatrix = defaultViewMatrix;
 async function main() { 
-	
     let carousel = false;
     const params = new URLSearchParams(location.search);
     
@@ -1286,16 +1285,11 @@ async function main() {
             movement[2] -= rightVector[2] * moveSpeed;
         }
 
-    	if (activeKeys.includes("KeyQ")) {
-		// 왼쪽으로 회전
-		rotationMatrix = rotate4(rotationMatrix, 0.01, 1, 10, 100);
-    	}
-    	if (activeKeys.includes("KeyE")) {
-	// 오른쪽으로 회전
-		rotationMatrix = rotate4(rotationMatrix, -0.01, 1, 10, 100);
-    	}
-	positionMatrix = translate4(positionMatrix, movement[0], movement[1], movement[2]);
-
+        if (activeKeys.includes("KeyQ")) inv = rotate4(inv, 0.01, 0, 0, 1);
+        if (activeKeys.includes("KeyE")) inv = rotate4(inv, -0.01, 0, 0, 1);
+	    
+        // 계산된 이동을 positionMatrix에 적용
+        positionMatrix = translate4(positionMatrix, movement[0], movement[1], movement[2]);
 
         // 이동된 positionMatrix와 회전된 rotationMatrix를 결합하여 viewMatrix 갱신
         viewMatrix = multiplyMatrices(positionMatrix, rotationMatrix);
